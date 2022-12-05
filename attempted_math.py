f450c25a77edd095b727c3bd174b52949ee642ff
Simplicity <68008162+dotzSimplicity@users.noreply.github.com>
2022-12-05 20:00:52 +0000

    never mind i fixed it, The withWhat checker was broken

--- attempted_math.py
+++ attempted_math.py
@@ -1,4 +1,5 @@
 import math # This is needed for checking square root
+import sys # This is needed for quitting the program
 
 print("Hello World!")
 print("It is time to do some Math!")
@@ -9,28 +10,25 @@ def withWhat_isnt_possible(): # This function gets called if withWhat isnt possi
     print("Either this feature isnt implemented or you wrote it wrong.")
     print("Console Calculator made by dotzSimplicity")
     print("This program has automatically exited.")
-    exit()
+    sys.exit()
 
 def is_withWhat_square_root(): # This function gets called if withWhat is "Square Root"
     print(math.sqrt(num1))
     print("Console Calculator made by dotzSimplicity")
-    exit()
+    sys.exit()
 
-possibleMath = ["Plus", "Times", "Division", "Minus", "Square Root"] # A List needed for a later.
+possibleMath = ["Plus", "Times", "Division", "Minus", "Square Root"] # A now unneeded List, This was used for checking is withWhat is possible
 
 num1 = int(input("First Number Please: ")) # Gets the First Number
 
 withWhat = input("Plus, Times, Division or Minus, Square Root? ") # Gets the wanted method of math.
 
-if withWhat != possibleMath: #This checks if withWhat is possible, If so it calls a function
-    withWhat_isnt_possible()
-
-if withWhat is "Square Root": #This checks if withWhat is "Square Root", If so it calls a function
+if withWhat == "Square Root": #This checks if withWhat is "Square Root", If so it calls a function
     is_withWhat_square_root()
 
 num2 = int(input("Second Number Please: ")) # Gets the second number
 
-if withWhat == "Plus": # If withWhat is "Plus", it adds num1 + num2
+if withWhat == "Plus": # If withWhat is "Plus", it adds num1 + num2, and so on
     print(num1 + num2)
 elif withWhat == "Times":
     print(num1 * num2)
@@ -39,4 +37,4 @@ elif withWhat == "Division":
 elif withWhat == "Minus":
     print(num1 - num2)
 
-print("Console Calculator made by dotzSimplicity")
+print("bad Console Calculator made by dotzSimplicity")
